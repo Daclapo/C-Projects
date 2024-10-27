bc1edbcabff884c901ade44d7168345787e9be0f
David Clarkson <daclapo148@gmail.com>
2024-10-27 14:36:15 +0100

    Added diffrente structures for printf

---
+++ 42Cursus/printf2/ft_printf_utils.c
@@ -0,0 +1,54 @@
+
+#include "ft_printf.h"
+
+int	ft_len_putstr_fd(char *s, int fd)
+{
+	int	len;
+
+	if (!s)
+		len = write(fd, "(null)", 6);
+	else
+		len = write(fd, s, ft_strlen(s));
+	return (len);
+}
+
+int	ft_len_put_nbase_fd(uintptr_t nbr, char *base)
+{
+	int			len;
+	uintptr_t	base_len;
+
+	base_len = ft_strlen(base);
+	len = 0;
+	if (nbr >= base_len)
+		ft_len_put_nbase_fd(nbr / base_len, base);
+	ft_putchar_fd(base[nbr % base_len], 1);
+	if (nbr == 0)
+		len++;
+	while (nbr > 0)
+	{
+		nbr /= base_len;
+		len++;
+	}
+	return (len);
+}
+
+int	ft_len_putnbr_fd(long int nbr, int fd)
+{
+	int		len;
+
+	len = 0;
+	ft_putnbr_fd(nbr, fd);
+	if (nbr == 0)
+		len++;
+	if (nbr < 0)
+	{
+		nbr *= (-1);
+		len++;
+	}
+	while (nbr > 0)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
+}
