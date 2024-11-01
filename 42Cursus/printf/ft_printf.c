e2e93080b8cc74caf8b3c421f3f64165e4c910b8
David Clarkson <adaclapo8@gmail.com>
2024-11-01 18:30:56 +0100

    printf 100%

--- 42Cursus/printf/ft_printf.c
+++ 42Cursus/printf/ft_printf.c
@@ -6,115 +6,82 @@
 /*   By: dclarkso <dclarkso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 17:25:37 by dclarkso          #+#    #+#             */
-/*   Updated: 2024/10/19 20:51:28 by dclarkso         ###   ########.fr       */
+/*   Updated: 2024/11/01 16:54:00 by dclarkso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putchar(char c)
+static void	process_char(char const *fstr, va_list args, int *count)
 {
-	write(1, &c, 1);
-	return (1);
+	int	aux;
+
+	if (*fstr == 'c')
+	{
+		aux = va_arg(args, int);
+		*count += write(1, &aux, 1);
+	}
+	else if (*fstr == 's')
+		ft_putstr(va_arg(args, char *), count);
 }
 
-int	ft_putstr(char *str)
+static void	process_nbr(char const *fstr, va_list args, int *count)
 {
-	int	i;
-
-	i = 0;
-	if (str == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
-	return (i);
+	if (*fstr == 'd' || *fstr == 'i')
+		ft_putnbr(va_arg(args, int), count);
+	else if (*fstr == 'u')
+		print_base(va_arg(args, unsigned int), "0123456789", 10, count);
+	else if (*fstr == 'x')
+		print_base(va_arg(args, unsigned int), "0123456789abcdef", 16, count);
+	else if (*fstr == 'X')
+		print_base(va_arg(args, unsigned int), "0123456789ABCDEF", 16, count);
 }
 
-int	ft_putnbr(int nb)
+static void	process_ptr(char const *fstr, va_list args, int *count)
 {
-	int	num_len;
+	unsigned long long	addr;
 
-	num_len = 0;
-	if (nb == -2147483648)
+	if (*fstr == 'p')
 	{
-		ft_putchar('-');
-		ft_putchar('2');
-		ft_putnbr(147483648);
-		num_len = 11;
-	}
-	else if (nb < 0)
-	{
-		ft_putchar('-');
-		ft_putnbr(-nb);
-		num_len = 1;
-	}
-	else if (nb > 9)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-		num_len++;
-	}
-	else
-		ft_putchar(nb + '0');
-		num_len++;
-	return (num_len);
-}
-
-int	conversion(va_list args, const char type_indicator)
-{
-	int	n_printed_chars;
-
-	n_printed_chars = 0;
-	if (type_indicator == 'c')
-		n_printed_chars += ft_putchar(va_arg(args, int));
-	else if (type_indicator == 's')
-		n_printed_chars += ft_putstr(va_arg(args, char *));
-
-	// %p El puntero void * dado como argumento se imprime en formato hexadecimal:
-	else if (type_indicator == 'p')
-		n_printed_chars += ft_putstr(va_arg(args, char *));
-
-	// %d Imprime un número decimal (base 10). %i Imprime un entero en base 10.
-	else if (type_indicator == 'd' || type_indicator == 'i')
-		n_printed_chars += ft_putnbr(va_arg(args, int));
-
-	// %u Imprime un número decimal (base 10) sin signo.
-	else if (type_indicator == 'u')
-		n_printed_chars += ft_putstr(va_arg(args, unsigned int));
-
-	// %x Imprime un número hexadecimal (base 16) en minúsculas. %X Imprime un número hexadecimal (base 16) en mayúsculas.
-	else if (type_indicator == 'x' || type_indicator == 'X')
-		n_printed_chars += ft_putstr(va_arg(args, unsigned int));
-	else if (type_indicator == '%')
-		n_printed_chars += ft_putchar('%');
-	return (n_printed_chars);
-}
-
-int	printf(const char *format, ...)
-{
-	va_list	args;
-	int		i;
-	int		char_to_print;
-
-	i = 0;
-	va_start(args, format);
-	while (format[i])
-	{
-		if (format[i] == '%')
+		addr = va_arg(args, unsigned long long);
+		if (addr == 0)
 		{
-			char_to_print = coversion(args, format[i + 1]);
-			i++;
+			*count += write(1, "(nil)", 5);
+			return ;
 		}
+		*count += write(1, "0x", 2);
+		print_base(addr, "0123456789abcdef", 16, count);
+	}
+}
+
+static void	parse_format(char const *fstr, va_list args, int *count)
+{
+	if (*fstr == 'c' || *fstr == 's')
+		process_char(fstr, args, count);
+	else if (*fstr == 'p')
+		process_ptr(fstr, args, count);
+	else if (*fstr == 'd' || *fstr == 'i' || *fstr == 'u'
+		|| *fstr == 'x' || *fstr == 'X')
+		process_nbr(fstr, args, count);
+	else if (*fstr == '%')
+		*count += write(1, "%", 1);
+}
+
+int	ft_printf(char const *fstr, ...)
+{
+	int		count;
+	va_list	args;
+
+	count = 0;
+	va_start(args, fstr);
+	while (*fstr)
+	{
+		if (*fstr == '%' && *(fstr + 1))
+			parse_format(++fstr, args, &count);
 		else
-			char_to_print = ft_putchar(format[i]);
-		i++;
+			count += write(1, fstr, 1);
+		++fstr;
 	}
 	va_end(args);
-	return (char_to_print);
+	return (count);
 }
--- 42Cursus/printf/ft_printf.h
+++ 42Cursus/printf/ft_printf.h
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf..h                                       :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dclarkso <dclarkso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:05:49 by dclarkso          #+#    #+#             */
-/*   Updated: 2024/10/19 17:01:22 by dclarkso         ###   ########.fr       */
+/*   Updated: 2024/11/01 14:48:40 by dclarkso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
 # include <stdarg.h>
 # include <unistd.h>
 
-int	ft_printf(const char *format, ...);
+int		ft_printf(char const *format, ...);
+void	ft_putnbr(int nbr, int *count);
+void	print_base(unsigned long long n, char *base, int base_len, int *count);
+void	ft_putstr(char *str, int *count);
 
 #endif
